1197bd56cc8cda7d1fc3d8c11dc6c3a21188a40f
Jeremy Sher <OverlappingElvis@gmail.com>
2016-12-18 00:44:36 -0800

    App updates.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+'use strict';
+
 var request = require('request');
 var _ = require('underscore');
+var similarity = require('string-similarity');
+var sbd = require('sbd');
 var AlexaSkill = require('./AlexaSkill');
 
-var APP_ID = 'amzn1.ask.skill.c27a692e-b588-4211-ab6e-26b4d8c40619';
+var APP_ID = process.env.appId;
 var apiUrl = 'https://ridb.recreation.gov/api/v1/';
 var ridbAuth = { apikey: process.env.ridbApiKey };
 var mapsAuth = { key: process.env.mapsApiKey };
 
+// RIDB expects a two-letter state code, so we want to check against them and map the state name if needed
 var states = {
 	abbr: ['al','ak','az','ar','ca','co','ct','de','fl','ga','hi','id','il','in','ia','ks','ky','la','me','md','ma','mi','mn','ms','mo','mt','ne','nv','nh','nj','nm','ny','nc','nd','oh','ok','or','pa','ri','sc','sd','tn','tx','ut','vt','va','wa','wv','wi','wy'],
 	fullName: {'alabama': 'al', 'alaska': 'ak', 'arizona': 'az', 'arkansas': 'ar', 'california': 'ca', 'colorado': 'co', 'connecticut': 'ct', 'delaware': 'de', 'florida': 'fl', 'georgia': 'ga', 'hawaii': 'hi', 'idaho': 'id', 'illinois': 'il', 'indiana': 'in', 'iowa': 'ia', 'kansas': 'ks', 'kentucky': 'ky', 'louisiana': 'la', 'maine': 'me', 'maryland': 'md', 'massachusetts': 'ma', 'michigan': 'mi', 'minnesota': 'mn', 'mississippi': 'ms', 'missouri': 'mo', 'montana': 'mt', 'nebraska': 'ne', 'nevada': 'nv', 'new hampshire': 'nh', 'new jersey': 'nj', 'new mexico': 'nm', 'new york': 'ny', 'north carolina': 'nc', 'north dakota': 'nd', 'ohio': 'oh', 'oklahoma': 'ok', 'oregon': 'or', 'pennsylvania': 'pa', 'rhode island': 'ri', 'south carolina': 'sc', 'south dakota': 'sd', 'tennessee': 'tn', 'texas': 'tx', 'utah': 'ut', 'vermont': 'vt', 'virginia': 'va', 'washington': 'wa', 'west virginia': 'wv', 'wisconsin': 'wi', 'wyoming': 'wy'} };
@@ -15,36 +20,8 @@ var RecreationSites = function () {
 	AlexaSkill.call(this, APP_ID);
 };
 
-// Extend AlexaSkill
-RecreationSites.prototype = Object.create(AlexaSkill.prototype);
-RecreationSites.prototype.constructor = RecreationSites;
-
-RecreationSites.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-	console.log('RecreationSites onSessionStarted requestId: ' + sessionStartedRequest.requestId
-		+ ', sessionId: ' + session.sessionId);
-	// any initialization logic goes here
-};
-
-RecreationSites.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-	console.log('RecreationSites onLaunch requestId: ' + launchRequest.requestId + ', sessionId: ' + session.sessionId);
-	getRecreationAreas(null, session, response);
-};
-
-RecreationSites.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-	console.log('RecreationSites onSessionEnded requestId: ' + sessionEndedRequest.requestId
-		+ ', sessionId: ' + session.sessionId);
-	// any cleanup logic goes here
-};
-
 var errorResponse = function(response) {
-	return response.tell('I\'m sorry, but I\'m unable to retrieve the information you requested.');
-};
-
-var reportCount = function(data, location, response) {
-	if (!data.METADATA) {
-		return errorResponse(response);
-	}
-	response.ask('I found ' + data.METADATA.RESULTS.TOTAL_COUNT + ' recreation areas ' + location + '. You can ask about a specific location for more details.', 'Try asking about a city or town.');
+	return response.tell('I\'m sorry, something went wrong with r. i. d. b.');
 };
 
 var getRecAreas = function() {
@@ -55,7 +32,13 @@ var getRecAreas = function() {
 	}, hasQuery ? arguments[1] : arguments[0]);
 };
 
-// Configure intents
+var reportCount = function(data, location, response) {
+	if (!data.METADATA) {
+		return errorResponse(response);
+	}
+	response.ask('I found ' + data.METADATA.RESULTS.TOTAL_COUNT + ' recreation areas ' + location + '. You can ask about a specific location for more details.', 'Try asking about a city or town.');
+};
+
 var getRecreationAreas = function(intent, session, response) {
 	getRecAreas(function(error, res, body) {
 		if (error) {
@@ -64,6 +47,25 @@ var getRecreationAreas = function(intent, session, response) {
 		}
 		reportCount(JSON.parse(body), 'nationwide', response);
 	});
+};
+
+// Extend AlexaSkill
+RecreationSites.prototype = Object.create(AlexaSkill.prototype);
+RecreationSites.prototype.constructor = RecreationSites;
+
+RecreationSites.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+	console.log('RecreationSites onSessionStarted requestId: ' + sessionStartedRequest.requestId + ', sessionId: ' + session.sessionId);
+	// any initialization logic goes here
+};
+
+RecreationSites.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+	console.log('RecreationSites onLaunch requestId: ' + launchRequest.requestId + ', sessionId: ' + session.sessionId);
+	getRecreationAreas(null, session, response);
+};
+
+RecreationSites.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+	console.log('RecreationSites onSessionEnded requestId: ' + sessionEndedRequest.requestId + ', sessionId: ' + session.sessionId);
+	// any cleanup logic goes here
 };
 
 var getRecreationAreasForState = function(intent, session, response) {
@@ -108,7 +110,7 @@ var getRecreationAreasForCity = function(intent, session, response) {
 		}
 		var parsed = JSON.parse(body);
 		if (!parsed.results.length) {
-			return response.ask('I wasn\'t able to find ' + cityName + '. Please ask again.', 'Say the name of a city or town.')
+			return response.ask('I wasn\'t able to find ' + cityName + '. Please ask again.', 'Say the name of a city or town.');
 		}
 		var location = parsed.results[0].geometry.location;
 		getRecAreas({
@@ -123,11 +125,49 @@ var getRecreationAreasForCity = function(intent, session, response) {
 			var parsed = JSON.parse(body);
 			var count = parsed.METADATA.RESULTS.TOTAL_COUNT;
 			if (!count) {
-				return response.tell('Sorry, I couldn\'t find any recreation areas within ' + radius + ' miles of ' + cityName);
+				return response.tell('Sorry, r.i.d.b. couldn\'t find any recreation areas within ' + radius + ' miles of ' + cityName);
 			}
-			response.tell('I found ' + parsed.METADATA.RESULTS.TOTAL_COUNT + ' recreation areas within ' + radius + ' miles of ' + cityName + '.');
+			var areas = parsed.RECDATA;
+			var sampledAreas = _(areas).sample(7);
+			var areasToSay = _(sampledAreas).pluck('RecAreaName').join(', ');
+			session.attributes.sampledAreas = sampledAreas;
+			session.attributes.count = count;
+			session.attributes.radius = radius;
+			response.ask('I found ' + count + ' recreation areas within ' + radius + ' miles of ' + cityName + '. Here are a few: ' + areasToSay, 'You can ask about a recreation site to get more information.');
 		});
 	});
+};
+
+// Get details for a specified recreation area
+var getRecreationArea = function(intent, session, response) {
+	var recAreaSlot = intent.slots.RecArea;
+	if (!recAreaSlot || !recAreaSlot.value) {
+		return errorResponse(response);
+	}
+	var recAreaName = recAreaSlot.value;
+	var sampledAreas = session.attributes.sampledAreas;
+	// reset session
+	session.attributes = {};
+	var match = similarity.findBestMatch(recAreaName, _(sampledAreas).pluck('RecAreaName'));
+	var name = match.bestMatch.target;
+	var matchingRecArea = _(sampledAreas).findWhere({
+		RecAreaName: name
+	});
+	if (!matchingRecArea) {
+		return response.ask('I didn\'t understand that recreation area name.',  'You can ask about ' + _(sampledAreas).pluck('RecAreaName').join(', '));
+	}
+	session.attributes.currentArea = matchingRecArea;
+	var description = matchingRecArea.RecAreaDescription;
+	response.ask(name + ': ' + sbd.sentences(description)[0] + ' Would you like directions to ' + name + '?');
+};
+
+var giveDirections = function(intent, session, response) {
+	var currentArea = session.attributes.currentArea;
+	if (!currentArea) {
+		return response.tell('I\'m sorry, I didn\'t understand that request');
+	}
+	var name = currentArea.RecAreaName;
+	response.tellWithCard('Okay, I\'m sending directions to the Alexa app. Enjoy ' + name + '!', 'Directions to ' + name, currentArea.RecAreaDirections);
 };
 
 RecreationSites.prototype.intentHandlers = {
@@ -135,8 +175,16 @@ RecreationSites.prototype.intentHandlers = {
 	GetRecreationAreas: getRecreationAreas,
 	GetRecreationAreasForState: getRecreationAreasForState,
 	GetRecreationAreasForCity: getRecreationAreasForCity,
+	GetRecreationArea: getRecreationArea,
+	'AMAZON.YesIntent': giveDirections,
+	'AMAZON.NoIntent': function(intent, session, response) {
+		if (!session.attributes.currentArea) {
+			return response.tell('I\'m sorry, I didn\'t understand that request');
+		}
+		response.tell('Okay. Have a great day!');
+	},
 	'AMAZON.HelpIntent': function (intent, session, response) {
-		response.ask('You can ask me about recreation areas near your location.', 'Try asking about your city or town.');
+		response.ask('Ask r.i.d.b. about recreation areas near you.', 'Try asking r.i.d.b. about your city or town.');
 	}
 };
 
